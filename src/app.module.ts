655d2ed465f52ea1ae50fb733327a4050b0a2e0d
chinemeze847 <thankgodchinemeze@gmail.com>
2023-08-07 07:35:44 +0100

    added the config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { BookModule } from './book/book.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 
 @Module({
-  imports: [BookModule, PrismaModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    BookModule, 
+    PrismaModule],
 })
 export class AppModule {}
