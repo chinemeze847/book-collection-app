668cb4c1fdc76e21e88e2a7de1c400d60fb6f984
chinemeze847 <thankgodchinemeze@gmail.com>
2023-08-07 06:52:46 +0100

    completed prisma config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { BookModule } from './book/book.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 
 @Module({
-  imports: [BookModule],
+  imports: [BookModule, PrismaModule],
 })
 export class AppModule {}
